cdb44bc44a3340ad4467148bf82b699b844419ee
Yaroslav <ravlushevich.iarek@mail.ru>
2023-05-10 16:57:29 +0300

    header media

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,16 @@
 import { animations } from './functions/animations.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-	
+	const burgerBtn = document.querySelector('.header__burger');
+	const headerBottomGroup = document.querySelector('.header__bottom-group');
+
+	burgerBtn.addEventListener('click', () => {
+		if(!burgerBtn.classList.contains('active')) {
+			burgerBtn.classList.add('active');
+			headerBottomGroup.classList.add('active');
+		} else {
+			burgerBtn.classList.remove('active');
+			headerBottomGroup.classList.remove('active');
+		}
+	});
 });
