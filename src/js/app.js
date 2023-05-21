2b703fa1520334157934df9dc164f49262e363b5
Yaroslav <ravlushevich.iarek@mail.ru>
2023-05-21 16:38:37 +0300

    add alert form

--- src/js/app.js
+++ src/js/app.js
@@ -49,4 +49,97 @@ document.addEventListener('DOMContentLoaded', () => {
 			});
 		}
 	});
+
+	/* FORMS */
+	const validateInputs = (inputs) => {
+			let k = 0;
+
+			inputs.forEach(input => {
+					if(!input.value) {
+							input.style.border = '1px solid red';
+					} else {
+							if(input.name === 'name' && /^[a-zA-Zа-яА-ЯёЁ ]+$/.test(input.value)) {
+									k++;
+									input.style.border = '1px solid #000';
+							} else if(input.name === 'phone' && /^[0-9]{9,13}$/.test(input.value)) {
+									k++;
+									input.style.border = '1px solid #000';
+							} else if(input.name === 'email' && /^[a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1}([a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1})*[a-zA-Z0-9]@[a-zA-Z0-9][-\.]{0,1}([a-zA-Z][-\.]{0,1})*[a-zA-Z0-9]\.[a-zA-Z0-9]{1,}([\.\-]{0,1}[a-zA-Z]){0,}[a-zA-Z0-9]{0,}$/i.test(input.value)) {
+									k++;
+									input.style.border = '1px solid #000';
+							} else if(input.name === 'message') {
+									k++;
+									input.style.border = '1px solid #000';
+							} else {
+									k--;
+									input.style.border = '1px solid red';
+							}
+					}
+			});
+
+			return k;
+	};
+
+	const resetInputs = (inputs) => {
+			inputs.forEach(input => {
+					input.value = '';
+			});
+	};
+
+	const sendData = (k, inputs, formClass) => {
+			if(inputs.length === k) {
+					const form = document.querySelector(`.${formClass}`);
+					let formData = new FormData(form); 
+					// console.log(form);
+
+					const formAlert = document.querySelector('.form-alert');
+					const alertCloseBtn = document.querySelector('.form-alert__close');
+
+					alertCloseBtn.addEventListener('click', () => {
+							formAlert.classList.remove('active');
+					});
+
+					fetch('assets/sendMail.php', {
+							method: 'POST',
+							body: formData
+					}).then(resp => {
+							resetInputs(inputs);
+							formAlert.classList.add('active');
+
+							if(formClass === 'contacts-modal__form') {
+									document.querySelector('.contacts-modal').classList.remove('modal--active');
+									document.body.style.overflow = 'auto';
+							}
+
+							setTimeout(() => {
+									formAlert.classList.remove('active');
+							}, 5000);
+					})
+			}
+	}
+
+	const formFunction = (formClass, btnClass) => {
+			const inputs = document.querySelectorAll(`.${formClass} input[type="text"], .${formClass} textarea`);
+			const btn = document.querySelector(`.${btnClass}`);
+			
+			if(btn) {
+					btn.addEventListener('click', (e) => {
+							e.preventDefault();
+
+							const k = validateInputs(inputs);
+
+							sendData(k, inputs, formClass);
+					});
+
+					inputs.forEach(input => {
+							input.addEventListener('input', () => {
+									validateInputs(inputs);
+							});
+					});
+			}
+	};
+
+	formFunction('contact-section__form', 'contact-section__submit');
+	formFunction('contacts__form', 'contacts__submit');
+	/* FORMS */
 });
